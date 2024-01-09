4509d948c6b272dcc4b30039daf227e26044669f
asp <asp@aptech.com>
2024-01-09 20:55:50 +0500

    second

---
+++ second.js
@@ -0,0 +1,51 @@
+let mydata = {
+    name : "peter",
+    city :"Karachi",
+    age : "56",
+    salary :"5666",
+    grade : "c"
+};
+
+//two methods of getting data form an objrct
+// first method
+
+console.log(mydata["city"]);
+console.log(mydata["age"]);
+
+//second method
+
+console.log(mydata.salary);
+console.log(mydata.grade);
+console.log(mydatay);
+
+
+//getting data with using loop
+
+for(let i in mydata){
+    console.log(i , mydata[i]);
+}
+// arthimetic operator
+
+let a = 45;
+let b = 74;
+
+
+console.log("the sum of a+b is , a+b");
+console.log("the sub of a+b is , a-b");
+console.log("the mul of a+b is , a*b");
+console.log("the div of a+b is , a/b");
+console.log("the modulus of a+b is , a%b");
+
+//logical opperator
+
+
+//or opertor
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+//and operator
+
+
