66ebb6cd9ac90ad060b0e188b521a4b7410e2f2d
asp <asp@aptech.com>
2024-01-09 20:23:54 +0500

    pjs

---
+++ first.js
@@ -0,0 +1,39 @@
+console.log("wellcome to pjs");
+console.log("hello world")
+
+// data types
+name = "mike";     //string
+age = "45";        //integer & nimber
+iscnic = "false"   //boolean true & false
+x = undefined;     //kuch nhi ha
+y = "null"        //empty
+
+
+console.table(name , age , iscnic , x ,y);
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(iscnic))
+console.log(typeof(x))
+console.log(typeof(y))
+
+//two types of data types
+
+// primitive , string , number ,boolean etc
+//non primitive arry , function , objects etc
+
+
+//three types of declaeration 
+//varvar can redecleared and update
+//letr can`t  be redecleared but  update
+//varvar can`t not redecleared and update
+
+var city = "karachi";
+console.log(city);
+city = "lahore";
+console.log(city);
+var city = "peshawar";
+console.log(city);
+
+let myage = 15;
+console.log(myage);
+myage = 80
