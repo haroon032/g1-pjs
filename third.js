83de4ae4a0adfbd7bed86b950c26d80c96ec604b
asp <asp@aptech.com>
2024-01-11 20:17:45 +0500

    third.js

---
+++ third.js
@@ -0,0 +1,31 @@
+let age = 20;
+
+if(age>=20){
+    console.log("You Can Cast Your Vote");
+}if(age <=20){
+    console.log("You Can't cast Your Vote")
+}
+
+
+if(age>=20){
+    console.log("You Can Cast Your Vote");
+}else{
+    console.log("You Can't cast Your Vote")
+}
+
+let mode = prompt(`Enter Your mode
+1) DARK
+2) LIGHT`);
+let color;
+if(mode === "DARK"){
+    color = "Black";
+}if(mode === "LIGHT"){
+    color = "Light";
+}
+    console.log("you choose the color" , color);
+    
+    
+    
+    
+    
+    
